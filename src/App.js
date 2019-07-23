6c9b72f18e96f4bcb8183ee0ea25948a317ca6d5
amaisuna <taro30143014@gmail.com>
2019-07-23 23:56:07 +0900

    more-jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1 className="App">Hello World!</h1> ;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {alert("aaaaburi")}} />
+    </React.Fragment>
+  )
 }
 
 export default App;
