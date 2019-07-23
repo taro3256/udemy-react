57ab7ff7db5774ecd8fb262587d05d52d1e1ff3f
amaisuna <taro30143014@gmail.com>
2019-07-24 01:16:16 +0900

    react props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    {name: "taro", age: 10},
+    {name: "hanako", age: 5},
+    {name: "noname"}
+  ]
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {alert("aaaaburi")}} />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-    </React.Fragment>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
   )
 }
 
-const Cat = () => {
-  return <div>こんにちは！</div>
+const User = (props) => {
+  return <div>こんにちは！ {props.name}！ 君は{props.age}歳！</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
