121e32abc9fd75b2a5178a1142377f35eca7bc99
amaisuna <taro30143014@gmail.com>
2019-07-23 23:32:20 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-        <p>
-          <h1>Hello World!</h1>
-        </p>
-    </div>
-  );
+  return <h1 className="App">Hello World!</h1> ;
 }
 
 export default App;
