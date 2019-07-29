7fd00e1dd147d265b00e179aba5a809b3b19360f
amaisuna <taro30143014@gmail.com>
2019-07-30 00:28:52 +0900

    auto commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 // export default combineReducers({ count, count2, count3 }) 複数の場合こんな風にかける
