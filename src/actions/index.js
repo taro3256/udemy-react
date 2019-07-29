b338814f7933a50894174713b1a37b9aee3c8da9
amaisuna <taro30143014@gmail.com>
2019-07-30 00:32:47 +0900

    auto commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,11 +6,13 @@ export const CREATE_EVENT = 'CREATE_EVENT'
 const ROOT_URL = 'http://udemy-utils.herokuapp.com/api/v1'
 const QUERYSRING = '?token=token123'
 
+// 一覧表示するイベント
 export const readEvents = () => async dispatch => {
     const responce = await axios.get(`${ROOT_URL}/events${QUERYSRING}`)
     dispatch({ type: READ_EVENTS, responce })
 }
 
+// 新規追加するイベント
 export const postEvent = values => async dispatch => {
     const responce = await axios.post(`${ROOT_URL}/events${QUERYSRING}`, values)
     dispatch({ type: CREATE_EVENT, responce })
